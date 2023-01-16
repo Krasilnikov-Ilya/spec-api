ef38760c2340a1d711e78432e411f41c37124549
i.krasilnikov <WtF3VgOmAo>
2023-01-17 01:58:02 +0400

    initial commit

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"gopkg.in/yaml.v3"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/spec", getSpec)
+	err := router.Run("localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getSpec(c *gin.Context) {
+	f, err := os.ReadFile("openapi.yaml")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var data map[string]interface{}
+
+	err = yaml.Unmarshal(f, &data)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET")
+
+	if c.Request.Method == "OPTIONS" {
+		c.AbortWithStatus(204)
+		return
+	}
+
+	c.JSON(http.StatusOK, data)
+}
